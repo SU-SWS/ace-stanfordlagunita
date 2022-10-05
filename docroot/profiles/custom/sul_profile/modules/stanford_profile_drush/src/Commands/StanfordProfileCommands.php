f406aa87e1a4a0c44718255c07fc07887116246c
Mike Decker <pookmish@gmail.com>
2022-10-05 19:14:04 +0000

    More frontend setup for gitpod

--- docroot/profiles/custom/sul_profile/modules/stanford_profile_drush/src/Commands/StanfordProfileCommands.php
+++ docroot/profiles/custom/sul_profile/modules/stanford_profile_drush/src/Commands/StanfordProfileCommands.php
@@ -87,6 +87,59 @@ class StanfordProfileCommands extends DrushCommands {
   }
 
   /**
+   * Set the secret string on the consumer entity.
+   * 
+   * @param string $uuid
+   *   Consumer entity uuid.
+   * @param string $secret
+   *   Consumer secret string.
+   *
+   * @command sul-profile:set-consumer-secret
+   */
+  public function setConsumerSecret(string $uuid, string $secret) {
+    $consumer_storage = $this->entityTypeManager->getstorage('consumer');
+    $consumer = $consumer_storage->loadByProperties(['uuid' => $uuid]);
+    if (empty($consumer)) {
+      $consumer_storage->create([
+        'uuid' => $uuid,
+        'label' => 'NextJS Consumer',
+        'description' => '',
+        'is_default' => TRUE,
+        'secret' => $secret,
+      ]);
+      return;
+    }
+    /** @var \Drupal\consumers\Entity\Consumer */
+    $consumer = reset($consumer);
+    $consumer->set('secret', $secret);
+    $consumer->save();
+  }
+
+  /**
+   * Create a NextJS site entity.
+   * 
+   * @param string $label
+   *   Label of the site entity.
+   * @param string $base_url
+   *   URL for the nextjs site.
+   * @param string $preview_url
+   *   Preview URL.
+   * @param string $preview_secret
+   *   Preview Secret.
+   * 
+   * @command sul-profile:create-nextjs-site
+   */
+  public function createNextJsSite(string $label, string $base_url, string $preview_url = NULL, string $preview_secret = NULL) {
+    $this->entityTypeManager->getStorage('next_site')->create([
+      'id' => preg_replace('/[^a-z\d]/', '_', strtolower($label)),
+      'label' => $label,
+      'base_url' => $base_url,
+      'preview_url' => $preview_url,
+      'preview_secret' => $preview_secret,
+    ])->save();
+  }
+
+  /**
    * Generate a page with the possible combinations of components in rows.
    *
    * @option name
