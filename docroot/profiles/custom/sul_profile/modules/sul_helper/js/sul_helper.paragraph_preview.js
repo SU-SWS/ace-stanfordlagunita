9d047428779a8d51195b0f2769e83ce172e7bf0e
Mike Decker <pookmish@gmail.com>
2022-11-02 13:48:11 -0700

    Refactored into a service

--- docroot/profiles/custom/sul_profile/modules/sul_helper/js/sul_helper.paragraph_preview.js
+++ docroot/profiles/custom/sul_profile/modules/sul_helper/js/sul_helper.paragraph_preview.js
@@ -4,9 +4,25 @@
       window.addEventListener('message', ({data}) => {
         const messageData = JSON.parse(data);
 
+        // If the returned data contains a message, do something with it.
+        if (messageData.message?.length > 1) {
+          switch (messageData.message) {
+            case 'refresh':
+
+              $('iframe.next-site-preview').each(function (i, iframe) {
+                iframe.contentWindow.postMessage($(iframe).attr('id'), '*');
+                iframe.contentWindow.postMessage($(iframe).attr('edit-data'), '*');
+              });
+
+              break;
+          }
+          return;
+        }
+
         $('#' + messageData.id).attr('height', messageData.height);
       });
 
+      // Post a message to the iframe containing all the data from the attributes.
       $('iframe.next-site-preview', context).each(function (i, iframe) {
         $(iframe).on('load', () => {
           setTimeout(() => {
