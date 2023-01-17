85bde0b963511675e3ff094d9d9992b7b02750d2
Mike Decker <pookmish@gmail.com>
2023-01-17 10:35:26 -0800

    Updated saml configs

--- simplesamlphp/config/acquia.config.php
+++ simplesamlphp/config/acquia.config.php
@@ -53,71 +53,54 @@ $config['auth.adminpassword'] = getenv('SAML_ADMIN_PASS');
  * $port = ':' . $_SERVER['SERVER_PORT'];
  * @endcode
  */
+// do Acquia specific translations here
+// Prevent Varnish from interfering with SimpleSAMLphp.
+// SSL terminated at the ELB / balancer so we correctly set the SERVER_PORT
+// and HTTPS for SimpleSAMLphp baseurl configuration.
+$protocol = 'http://';
+$port = '80';
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $_SERVER['SERVER_PORT'] = 443;
+  $_SERVER['HTTPS'] = 'true';
+  $protocol = 'https://';
+  $port = $_SERVER['SERVER_PORT'];
+}
+$config['baseurlpath'] = $protocol . $_SERVER['HTTP_HOST'] . ':' . $port . '/simplesaml/';
+$config['trusted.url.domains'] = [$_SERVER['HTTP_HOST']];
 
-/**
- * Cookies No Cache.
- *
- * Allow users to be automatically logged in if they signed in via the same
- * SAML provider on another site by uncommenting the setcookie line below.
- *
- * Warning: This has performance implications for anonymous users.
- *
- * @link https://docs.acquia.com/resource/using-simplesamlphp-acquia-cloud-site
- *
- * @code
- * setcookie('NO_CACHE', '1');
- * @endcode
- */
-
-/**
- * Generate Acquia session storage via hosting creds.json.
- *
- * Session sorage defaults using the database for the current request.
- *
- * @link https://docs.acquia.com/resource/using-simplesamlphp-acquia-cloud-site/#storing-session-information-using-the-acquia-cloud-sql-database
- */
-
-// Support multi-site and single site installations at different base URLs.
-// Overide $config['baseurlpath'] = "https://{yourdomain}/simplesaml/"
-// to customize the default Acquia configuration.
-// phpcs:ignore
-$config['baseurlpath'] = $protocol . $_SERVER['HTTP_HOST'] . $port . '/simplesaml/';
-// Set ACE and ACSF sites based on hosting database and site name.
-$config['certdir'] = "/mnt/www/html/{$_ENV['AH_SITE_GROUP']}.{$_ENV['AH_SITE_ENVIRONMENT']}/simplesamlphp/cert/";
-$config['metadatadir'] = "/mnt/www/html/{$_ENV['AH_SITE_GROUP']}.{$_ENV['AH_SITE_ENVIRONMENT']}/simplesamlphp/metadata";
-$config['baseurlpath'] = 'simplesaml/';
-// Setup basic logging.
+// Setup basic file based logging.
 $config['logging.handler'] = 'file';
-// phpcs:ignore
+// on Cloud Next, the preferred location is /shared/logs
+// on Cloud Classic, the preferred location is the same directory as ACQUIA_HOSTING_DRUPAL_LOG
 $config['loggingdir'] = (file_exists('/shared/logs/')) ? '/shared/logs/' : dirname(getenv('ACQUIA_HOSTING_DRUPAL_LOG'));
 $config['logging.logfile'] = 'simplesamlphp-' . date('Ymd') . '.log';
-$creds_json = file_get_contents('/var/www/site-php/' . $_ENV['AH_SITE_GROUP'] . '.' . $_ENV['AH_SITE_ENVIRONMENT'] . '/creds.json');
-$databases = json_decode($creds_json, TRUE);
-$creds = $databases['databases'][$_ENV['AH_SITE_GROUP']];
 
-// On Cloud Classic, the current active database host is determined by a DNS lookup,
-// so we add a DNS resolver if and only if we are on Cloud Classic.
-if (isset($creds['db_cluster_id'])) {
+// Retrieve database credentials from creds.json
+$creds_json = file_get_contents('/var/www/site-php/' . $_ENV['AH_SITE_GROUP'] . '.' . $_ENV['AH_SITE_ENVIRONMENT'] . '/creds.json');
+$creds = json_decode($creds_json, TRUE);
+
+$database = $creds['databases'][$_ENV['AH_SITE_GROUP']];
+// On Cloud Classic, the current active database host is determined by a DNS lookup
+if (isset($database['db_cluster_id'])) {
   require_once "/usr/share/php/Net/DNS2_wrapper.php";
   try {
     $resolver = new Net_DNS2_Resolver([
-      'local_host' => '127.0.0.1',
       'nameservers' => [
         '127.0.0.1',
         'dns-master',
       ],
     ]);
-    $response = $resolver->query("cluster-{$creds['db_cluster_id']}.mysql", 'CNAME');
-    $creds['host'] = $response->answer[0]->cname;
+    $response = $resolver->query("cluster-{$database['db_cluster_id']}.mysql", 'CNAME');
+    $database['host'] = $response->answer[0]->cname;
   }
   catch (Net_DNS2_Exception $e) {
     Logger::warning('DNS entry not found');
   }
 }
 $config['store.type'] = 'sql';
-$config['store.sql.dsn'] = sprintf('mysql:host=%s;port=%s;dbname=%s', $creds['host'], $creds['port'], $creds['name']);
-$config['store.sql.username'] = $creds['user'];
-$config['store.sql.password'] = $creds['pass'];
+$config['store.sql.dsn'] = sprintf('mysql:host=%s;port=%s;dbname=%s', $database['host'], $database['port'], $database['name']);
+$config['store.sql.username'] = $database['user'];
+$config['store.sql.password'] = $database['pass'];
 $config['store.sql.prefix'] = 'simplesaml';
 
 $config['certdir'] = EnvironmentDetector::getAhFilesRoot() . '/nobackup/simplesamlphp/';
