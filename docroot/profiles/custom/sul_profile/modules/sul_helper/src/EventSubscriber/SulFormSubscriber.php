4b3f19fa85b865edb18cc928d230b481ba349739
Mike Decker <pookmish@gmail.com>
2023-03-08 13:48:57 -0800

    SUL23-159 Validate card paragraph for circular references

--- docroot/profiles/custom/sul_profile/modules/sul_helper/src/EventSubscriber/SulFormSubscriber.php
+++ docroot/profiles/custom/sul_profile/modules/sul_helper/src/EventSubscriber/SulFormSubscriber.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\sul_helper\EventSubscriber;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\core_event_dispatcher\Event\Form\FormIdAlterEvent;
+use Drupal\field_event_dispatcher\Event\Field\WidgetCompleteFormAlterEvent;
+use Drupal\field_event_dispatcher\FieldHookEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -16,6 +19,7 @@ class SulFormSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
     return [
       'hook_event_dispatcher.form_layout_paragraphs_component_form.alter' => ['layoutParagraphComponentFormAlter'],
+      FieldHookEvents::WIDGET_COMPLETE_FORM_ALTER => ['onWidgetFormAlter'],
     ];
   }
 
@@ -39,6 +43,45 @@ class SulFormSubscriber implements EventSubscriberInterface {
       $form['sul_contact__phone']['#states'] = $state;
       $form['sul_contact__title']['#states'] = $state;
 
+    }
+  }
+
+  /**
+   * Widget form alter event.
+   *
+   * @param \Drupal\field_event_dispatcher\Event\Field\WidgetCompleteFormAlterEvent $event
+   *   Triggered event.
+   */
+  public function onWidgetFormAlter(WidgetCompleteFormAlterEvent $event): void {
+    $context = $event->getContext();
+    if ($context['items']->getName() == 'sul_contact__branch') {
+      $widget_form = &$event->getWidgetCompleteForm();
+      $widget_form['widget']['#element_validate'][] = [
+        self::class,
+        'validateBranchField',
+      ];
+    }
+  }
+
+  /**
+   * Validate the branch field for contact card paragraphs.
+   *
+   * @param array $element
+   *   Field widget form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Form state.
+   * @param array $complete_form
+   *   Complete entity form.
+   */
+  public static function validateBranchField(array $element, FormStateInterface $form_state, array $complete_form): void {
+    /** @var \Drupal\layout_paragraphs\Form\EditComponentForm $object */
+    $object = $form_state->getBuildInfo()['callback_object'];
+    $value = $form_state->getValue($element['#parents']);
+    $parent_entity = $object->getParagraph()->getParentEntity();
+    foreach ($value as $target) {
+      if ($parent_entity && $target['target_id'] == $parent_entity->id()) {
+        $form_state->setError($element, 'This would create circular reference. Please create custom contact information.');
+      }
     }
   }
 
