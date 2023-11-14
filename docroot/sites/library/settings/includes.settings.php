f62bacfaae7b89180a7d8ce7f84ffa191ed87d47
Mike Decker <pookmish@gmail.com>
2023-11-13 20:06:46 -0800

    Update next preview settings

--- docroot/sites/library/settings/includes.settings.php
+++ docroot/sites/library/settings/includes.settings.php
@@ -18,7 +18,7 @@ elseif (EnvironmentDetector::isLocalEnv()) {
 if ($next_domain) {
   $config['next.next_site.netlify'] = [
     'base_url' => $next_domain,
-    'preview_url' => "$next_domain/api/preview",
+    'preview_url' => "$next_domain/api/draft",
     'revalidate_url' => "$next_domain/api/revalidate",
   ];
 }
