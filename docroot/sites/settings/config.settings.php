f5547488252a21b9fe12d06b007e916f68282db7
Mike Decker <pookmish@gmail.com>
2024-01-31 08:44:49 -0800

    Block search engines on non-prod

--- docroot/sites/settings/config.settings.php
+++ docroot/sites/settings/config.settings.php
@@ -9,6 +9,7 @@ use Acquia\Blt\Robo\Common\EnvironmentDetector;
 
 if (!EnvironmentDetector::isProdEnv()) {
   $config['domain_301_redirect.settings']['enabled'] = FALSE;
+  $settings['nobots'] = TRUE;
 }
 
 if (EnvironmentDetector::isAhEnv()) {
