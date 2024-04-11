b5c0873dabcad0ef72ff6c24efc191dd0cc69a72
Mike Decker <pookmish@gmail.com>
2024-04-11 07:57:59 -0700

    Added and configured stage file proxy for non-prod environments

--- docroot/sites/settings/config.settings.php
+++ docroot/sites/settings/config.settings.php
@@ -10,6 +10,16 @@ use Acquia\Blt\Robo\Common\EnvironmentDetector;
 if (!EnvironmentDetector::isProdEnv()) {
   $config['domain_301_redirect.settings']['enabled'] = FALSE;
   $settings['nobots'] = TRUE;
+
+  /**
+   * This is always set and exposed by the Acquia BLT.
+   *
+   * @var string $site_name
+   */
+  $config['stage_file_proxy.settings'] = [
+    'origin' => "https://$site_name.sites-pro.stanford.edu",
+    'origin_dir' => "sites/$site_name/files",
+  ];
 }
 
 if (EnvironmentDetector::isAhEnv()) {
