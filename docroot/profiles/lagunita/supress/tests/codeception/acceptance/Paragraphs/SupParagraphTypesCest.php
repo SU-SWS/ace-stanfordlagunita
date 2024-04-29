9b4f72870081c64eb5a389d1736547de8b27504c
pookmish <pookmish@gmail.com>
2024-04-30 02:44:22 +0700

    SUP-198 SUP-181 SUP-174 Blog teaser, Search Form, and Carousel paragraphs (#89)

--- docroot/profiles/lagunita/supress/tests/codeception/acceptance/Paragraphs/SupParagraphTypesCest.php
+++ docroot/profiles/lagunita/supress/tests/codeception/acceptance/Paragraphs/SupParagraphTypesCest.php
@@ -13,7 +13,8 @@ class SupParagraphTypesCest {
     $I->amOnPage('/admin/structure/paragraphs_type');
     $I->canSee('sup_carousel');
     $I->canSee('sup_file_list');
-    $I->canSee('sup_pre_built');
+    $I->canSee('Blog Teasers');
+    $I->canSee('Blog Item');
     $I->canSee('sup_carousel_slide');
   }
 
