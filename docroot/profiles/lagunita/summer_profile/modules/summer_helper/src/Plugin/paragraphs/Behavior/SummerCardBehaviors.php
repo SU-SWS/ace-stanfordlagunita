63e3669756d76c2dab2d299eb987b7ff5aa08537
Marc Young <mdyoung3@stanford.edu>
2024-05-08 14:13:17 -0600

    SUM-113 - Pill Card variant (#94)

---
+++ docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerCardBehaviors.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\summer_helper\Plugin\paragraphs\Behavior;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\stanford_paragraph_card\Plugin\paragraphs\Behavior\CardBehavior;
+
+/**
+ * Add desired behaviors to paragraph.
+ *
+ * @codeCoverageIgnore No functionality to test.
+ */
+class SummerCardBehaviors extends CardBehavior {
+
+  /**
+   * Builds card behavior form for a paragraph.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The paragraph entity object.
+   * @param array $form
+   *   An associative array representing the form structure.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   An updated associative array representing the form structure with the
+   *   added 'card_variant' element.
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state): array {
+    $element = parent::buildBehaviorForm($paragraph, $form, $form_state);
+    $element['sum_card_variant'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Card variant'),
+      '#empty_option' => $this->t('Default'),
+      '#options' => [
+        'pill' => $this->t('Pill'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting('su_card_styles', 'sum_card_variant'),
+    ];
+
+    return $element;
+  }
+
+}
