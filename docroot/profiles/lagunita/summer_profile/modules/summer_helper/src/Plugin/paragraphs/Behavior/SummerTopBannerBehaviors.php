e5e338daf1355655b0d0ad48a799a0f3b7908e84
Marc Young <mdyoung3@stanford.edu>
2024-05-15 17:53:40 -0600

    Fixed merge conflict rebase.

---
+++ docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerTopBannerBehaviors.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\summer_helper\Plugin\paragraphs\Behavior;
+
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+use Drupal\stanford_paragraph_card\Plugin\paragraphs\Behavior\CardBehavior;
+
+/**
+ * Add desired behaviors to paragraph.
+ *
+ * @codeCoverageIgnore No functionality to test.
+ */
+class SummerTopBannerBehaviors extends ParagraphsBehaviorBase {
+  /**
+   * {@inheritDoc}
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type): bool {
+    return $paragraphs_type->id() == 'sum_top_banner';
+  }
+
+  /**
+   * Builds card behavior form for a paragraph.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The paragraph entity object.
+   * @param array $form
+   *   An associative array representing the form structure.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   An updated associative array representing the form structure with the
+   *   added 'card_variant' element.
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state): array {
+    $element = parent::buildBehaviorForm($paragraph, $form, $form_state);
+    $element['sum_top_banner_alignment'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Banner Alignment'),
+      '#empty_option' => $this->t('Default'),
+      '#options' => [
+        'left' => $this->t('Left'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting('su_top_banner_styles', 'sum_top_banner_alignment'),
+    ];
+
+    return $element;
+  }
+
+  public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
+    // TODO: Implement view() method.
+  }
+
+}
