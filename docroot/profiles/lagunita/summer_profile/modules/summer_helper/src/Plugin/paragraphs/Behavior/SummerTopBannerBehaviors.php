da062ce442a91f88c5dd9ebcde549714426683b0
Marc Young <mdyoung3@stanford.edu>
2024-05-16 09:52:58 -0600

    Removed top banner variant

--- docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerTopBannerBehaviors.php
+++ docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerTopBannerBehaviors.php
@@ -48,17 +48,6 @@ class SummerTopBannerBehaviors extends ParagraphsBehaviorBase {
       ],
       '#default_value' => $paragraph->getBehaviorSetting('su_top_banner_styles', 'sum_top_banner_alignment'),
     ];
-    $element['sum_top_banner_background_color'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Banner Background Color'),
-      '#empty_option' => $this->t('Poppy Light'),
-      '#options' => [
-        'olive' => $this->t('Olive Light'),
-        'spirited_light' => $this->t('Spirited Light'),
-        'spirited_dark' => $this->t('Spirited Dark'),
-      ],
-      '#default_value' => $paragraph->getBehaviorSetting('su_top_banner_styles', 'sum_top_banner_background_color'),
-    ];
 
     return $element;
   }
