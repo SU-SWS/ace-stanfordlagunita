1bd00ecfdae9e80478fc08731ef9070ff648e63b
Marc Young <mdyoung3@stanford.edu>
2024-05-30 16:37:29 -0600

    Added corrected prefix to paragraph
    Added corrected prefix to paragraph

--- docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerFactsBehaviors.php
+++ docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerFactsBehaviors.php
@@ -2,9 +2,17 @@
 
 namespace Drupal\summer_helper\Plugin\paragraphs\Behavior;
 
+use Drupal\paragraphs\Entity\ParagraphsType;
 use Drupal\paragraphs\ParagraphsBehaviorBase;
 
 class SummerTopBannerBehaviors extends ParagraphsBehaviorBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type) {
+    return $paragraphs_type->id() == 'stanford_card';
+  }
+
   
 }
 
