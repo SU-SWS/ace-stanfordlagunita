640ed2c765f7ae51ee27019223ea7a98c4ca8cc4
Marc Young <mdyoung3@stanford.edu>
2024-05-30 15:49:12 -0600

    git rebase merge conflict

---
+++ docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerFactsBehaviors.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\summer_helper\Plugin\paragraphs\Behavior;
+
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+
+class SummerTopBannerBehaviors extends ParagraphsBehaviorBase {
+  
+}
+
