b96d15826ccf57e24371f531a4bc5fe4ee7410fe
Marc Young <marcdeeyoung@gmail.com>
2024-06-04 14:56:52 -0600

    SUM-112 - Pill banner paragraph component (#114)

--- docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerCarouselBehaviors.php
+++ docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerCarouselBehaviors.php
@@ -12,6 +12,8 @@ use Drupal\paragraphs\ParagraphsBehaviorBase;
 /**
  * Manages and controlling the behavior and display of "Carousel" element.
  *
+ * @codeCoverageIgnore
+ *
  * @ParagraphsBehavior(
  *   id = "sum_carousel",
  *   label = @Translation("Summer Carousel Behavior"),
--- docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerPillBannerBehaviors.php
+++ docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerPillBannerBehaviors.php
@@ -16,19 +16,19 @@ use Drupal\paragraphs\ParagraphsBehaviorBase;
  * @codeCoverageIgnore
  *
  * @ParagraphsBehavior(
- *   id = "sum_banner_behaviors",
- *   label = @Translation("Summer Banner Component Behavior"),
- *   description = @Translation("This plugin offers banner variants."),
+ *   id = "sum_pill_banner_behaviors",
+ *   label = @Translation("Summer Pill Banner Component Behavior"),
+ *   description = @Translation("This plugin offers pill banner variants."),
  *   weight = 0,
  * )
  */
-class SummerBannerBehaviors extends ParagraphsBehaviorBase {
+class SummerPillBannerBehaviors extends ParagraphsBehaviorBase {
 
   /**
    * {@inheritdoc}
    */
   public static function isApplicable(ParagraphsType $paragraphs_type) {
-    return $paragraphs_type->id() == 'stanford_banner';
+    return $paragraphs_type->id() == 'sum_pill_banner';
   }
 
   /**
@@ -36,30 +36,28 @@ class SummerBannerBehaviors extends ParagraphsBehaviorBase {
    */
   public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state): array {
     $element = parent::buildBehaviorForm($paragraph, $form, $form_state);
-    $element['sum_banner_alignment'] = [
+    $element['sum_pill_banner_overlay_bkg'] = [
       '#type' => 'select',
-      '#title' => $this->t('Overlay Alignment'),
-      '#empty_option' => $this->t('Right'),
-      '#options' => [
-        'left' => $this->t('Left'),
-      ],
-      '#default_value' => $paragraph->getBehaviorSetting('sum_banner_behaviors', 'sum_banner_alignment'),
-    ];
-    $element['sum_banner_overlay_bkg'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Banner Background Color'),
+      '#title' => $this->t('Pill Banner Background Color'),
       '#empty_option' => $this->t('Poppy Light'),
       '#options' => [
         'olive' => $this->t('Olive Light'),
         'spirited' => $this->t('Spirited Light'),
         'spirited_dark' => $this->t('Spirited Dark'),
+        'white' => $this->t('White'),
       ],
-      '#default_value' => $paragraph->getBehaviorSetting('sum_banner_behaviors', 'sum_banner_overlay_bkg'),
+      '#default_value' => $paragraph->getBehaviorSetting('sum_pill_banner_behaviors', 'sum_pill_banner_overlay_bkg'),
     ];
-    $element['sum_banner_button'] = [
+    $element['sum_pill_banner_oval_bkgd'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Semi-circle Button'),
-      '#default_value' => $paragraph->getBehaviorSetting('sum_banner_behaviors', 'sum_banner_button'),
+      '#title' => $this->t('Add oval background'),
+      '#default_value' => $paragraph->getBehaviorSetting('sum_pill_banner_behaviors', 'sum_banner_button'),
+    ];
+    $element['sum_pill_banner_copy_size'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Alternative font size'),
+      '#description' => $this->t('Change font size for the description.'),
+      '#default_value' => $paragraph->getBehaviorSetting('sum_pill_banner_behaviors', 'sum_pill_banner_copy_size'),
     ];
 
     return $element;
