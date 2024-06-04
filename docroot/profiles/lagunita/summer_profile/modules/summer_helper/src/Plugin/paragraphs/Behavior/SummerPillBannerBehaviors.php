0ff3019c1446be1275b2ecbe60f31ee8efe30652
Marc Young <marcdeeyoung@gmail.com>
2024-06-04 11:30:06 -0600

    Pill paragraph fields and variant

---
+++ docroot/profiles/lagunita/summer_profile/modules/summer_helper/src/Plugin/paragraphs/Behavior/SummerPillBannerBehaviors.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\summer_helper\Plugin\paragraphs\Behavior;
+
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Annotation\ParagraphsBehavior;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+
+/**
+ * Manages and controlling the behavior and display of banner component.
+ *
+ * @ParagraphsBehavior(
+ *   id = "sum_pill_banner_behaviors",
+ *   label = @Translation("Summer Pill Banner Component Behavior"),
+ *   description = @Translation("This plugin offers pill banner variants."),
+ *   weight = 0,
+ * )
+ */
+class SummerPillBannerBehaviors extends ParagraphsBehaviorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type) {
+    return $paragraphs_type->id() == 'sum_pill_banner';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state): array {
+    $element = parent::buildBehaviorForm($paragraph, $form, $form_state);
+    $element['sum_pill_banner_overlay_bkg'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Pill Banner Background Color'),
+      '#empty_option' => $this->t('Poppy Light'),
+      '#options' => [
+        'olive' => $this->t('Olive Light'),
+        'spirited' => $this->t('Spirited Light'),
+        'spirited_dark' => $this->t('Spirited Dark'),
+        'white' => $this->t('White'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting('sum_banner_behaviors', 'sum_pill_banner_overlay_bkg'),
+    ];
+    $element['sum_pill_banner_oval_bkgd'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Add oval background'),
+      '#default_value' => $paragraph->getBehaviorSetting('sum_banner_behaviors', 'sum_banner_button'),
+    ];
+
+    return $element;
+  }
+
+  /**
+   * Included to view the top banner.
+   */
+  public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
+    // Simple changes for the edit form.
+  }
+
+}
