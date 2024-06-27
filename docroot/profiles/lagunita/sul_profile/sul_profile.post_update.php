f65213bd528d394d43eb4e5314903b091d293691
Mike Decker <pookmish@gmail.com>
2024-06-27 11:06:13 -0700

    SUP & SUM: Added some wysiwyg styles

--- docroot/profiles/lagunita/sul_profile/sul_profile.post_update.php
+++ docroot/profiles/lagunita/sul_profile/sul_profile.post_update.php
@@ -10,16 +10,16 @@
  */
 function  sul_profile_removed_post_updates() {
   return [
-    ' sul_profile_post_update_8001' => '8.x-1.13',
-    ' sul_profile_post_update_8003' => '8.x-1.13',
-    ' sul_profile_post_update_8013' => '8.x-1.13',
-    ' sul_profile_post_update_8014' => '8.x-2.9',
-    ' sul_profile_post_update_8015' => '8.x-2.9',
-    ' sul_profile_post_update_8200' => '11.4.0',
-    ' sul_profile_post_update_8201' => '11.4.0',
-    ' sul_profile_post_update_8202' => '11.4.0',
-    ' sul_profile_post_update_update_field_defs' => '11.4.0',
-    ' sul_profile_post_update_samlauth' => '11.4.0',
-    ' sul_profile_post_update_site_orgs' => '11.4.0',
+    'sul_profile_post_update_8001' => '8.x-1.13',
+    'sul_profile_post_update_8003' => '8.x-1.13',
+    'sul_profile_post_update_8013' => '8.x-1.13',
+    'sul_profile_post_update_8014' => '8.x-2.9',
+    'sul_profile_post_update_8015' => '8.x-2.9',
+    'sul_profile_post_update_8200' => '11.4.0',
+    'sul_profile_post_update_8201' => '11.4.0',
+    'sul_profile_post_update_8202' => '11.4.0',
+    'sul_profile_post_update_update_field_defs' => '11.4.0',
+    'sul_profile_post_update_samlauth' => '11.4.0',
+    'sul_profile_post_update_site_orgs' => '11.4.0',
   ];
 }
--- docroot/profiles/lagunita/supress/supress.post_update.php
+++ docroot/profiles/lagunita/supress/supress.post_update.php
@@ -10,16 +10,16 @@
  */
 function supress_removed_post_updates() {
   return [
-    ' supress_post_update_8001' => '8.x-1.13',
-    ' supress_post_update_8003' => '8.x-1.13',
-    ' supress_post_update_8013' => '8.x-1.13',
-    ' supress_post_update_8014' => '8.x-2.9',
-    ' supress_post_update_8015' => '8.x-2.9',
-    ' supress_post_update_8200' => '11.4.0',
-    ' supress_post_update_8201' => '11.4.0',
-    ' supress_post_update_8202' => '11.4.0',
-    ' supress_post_update_update_field_defs' => '11.4.0',
-    ' supress_post_update_samlauth' => '11.4.0',
-    ' supress_post_update_site_orgs' => '11.4.0',
+    'supress_post_update_8001' => '8.x-1.13',
+    'supress_post_update_8003' => '8.x-1.13',
+    'supress_post_update_8013' => '8.x-1.13',
+    'supress_post_update_8014' => '8.x-2.9',
+    'supress_post_update_8015' => '8.x-2.9',
+    'supress_post_update_8200' => '11.4.0',
+    'supress_post_update_8201' => '11.4.0',
+    'supress_post_update_8202' => '11.4.0',
+    'supress_post_update_update_field_defs' => '11.4.0',
+    'supress_post_update_samlauth' => '11.4.0',
+    'supress_post_update_site_orgs' => '11.4.0',
   ];
 }
