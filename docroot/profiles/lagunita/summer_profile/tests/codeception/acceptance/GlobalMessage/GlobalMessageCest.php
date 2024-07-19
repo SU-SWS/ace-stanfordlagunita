e58e33ec03b2774938ce7b5c7022ae8324ce191a
Marc Young <mdyoung3@stanford.edu>
2024-07-19 13:32:23 -0600

    Just setting to protected

--- docroot/profiles/lagunita/summer_profile/tests/codeception/acceptance/GlobalMessage/GlobalMessageCest.php
+++ docroot/profiles/lagunita/summer_profile/tests/codeception/acceptance/GlobalMessage/GlobalMessageCest.php
@@ -42,6 +42,8 @@ class GlobalMessageCest {
     $I->logInWithRole('administrator');
     $I->amOnPage('/admin/config/system/global-message');
     $I->checkOption('#edit-su-global-msg-enabled-value');
+    $I->selectOption("#edit-su-global-msg-type", "success");
+    $I->fillField('Label', 'MESSAGE LABEL');
     $I->fillField('Headline', 'MESSAGE HEADER');
     $I->fillField('#edit-su-global-msg-message-0-value', '<p>This is the message body.</p>');
     $I->fillField('URL', '<front>');
@@ -50,12 +52,18 @@ class GlobalMessageCest {
     $I->see('Global Message has been', '.messages-list');
 
     $I->amOnPage("/");
+    $I->seeElement(".su-alert--success");
+    $I->canSee("MESSAGE LABEL");
     $I->canSee("MESSAGE HEADER");
     $I->canSee("This is the message body");
     $I->canSee("Action link");
+    $I->amOnPage('/admin/config/system/global-message');
+    $I->selectOption("#edit-su-global-msg-type", "error");
     $I->click('Save');
     $I->see('Global Message has been', '.messages-list');
+
     $I->amOnPage("/");
+    $I->seeElement(".su-alert--error");
   }
 
   /**
