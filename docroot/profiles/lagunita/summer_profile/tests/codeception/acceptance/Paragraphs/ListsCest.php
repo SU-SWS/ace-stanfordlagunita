e865188d32398492bf7e1e916e9e058ad0036c9f
Mike Decker <pookmish@stanford.edu>
2024-08-09 10:59:05 -0700

    Flush caches in test

--- docroot/profiles/lagunita/summer_profile/tests/codeception/acceptance/Paragraphs/ListsCest.php
+++ docroot/profiles/lagunita/summer_profile/tests/codeception/acceptance/Paragraphs/ListsCest.php
@@ -783,7 +783,7 @@ class ListsCest {
       'items_to_display' => 100,
       'arguments' => 'Basic-Page-Test-Term',
     ]);
-    $I->runDrush('cr');
+    drupal_flush_all_caches();
     $I->amOnPage($node->toUrl()->toString());
     $I->canSee($basic_page_entity->label(), 'h3');
     $I->canSee($second_basic_page_entity->label(), 'h3');
@@ -805,7 +805,6 @@ class ListsCest {
     ]);
     $I->amOnPage($layout_changed_page->toUrl('edit-form')->toString());
     $I->click('Save');
-    $I->runDrush('cr');
     $I->amOnPage($node->toUrl()->toString());
     $I->canSee($layout_changed_page->label(), 'h3');
     $I->canSee($layout_changed_page->get('su_page_description')->getString());
@@ -833,7 +832,7 @@ class ListsCest {
       'items_to_display' => 100,
       'arguments' => 'Basic-Page-Test-Term',
     ]);
-    $I->runDrush('cr');
+
     $I->amOnPage($node->toUrl()->toString());
     $I->canSee($basic_page_entity->label(), 'h3');
     $I->canSee($second_basic_page_entity->label(), 'h3');
