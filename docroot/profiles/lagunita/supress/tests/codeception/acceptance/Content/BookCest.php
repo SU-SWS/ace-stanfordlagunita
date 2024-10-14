b1cfee2ef4329193d41f8c20b25a790f38e53c01
Mike Decker <pookmish@stanford.edu>
2024-10-14 12:11:13 -0700

    Adjusted test

--- docroot/profiles/lagunita/supress/tests/codeception/acceptance/Content/BookCest.php
+++ docroot/profiles/lagunita/supress/tests/codeception/acceptance/Content/BookCest.php
@@ -24,7 +24,6 @@ class BookCest {
 
   /**
    * Ensure the content type exist and has its fields
-   *
    */
   public function testBookContentType(AcceptanceTester $I) {
     // content type exists
@@ -32,61 +31,42 @@ class BookCest {
     $I->amOnPage('/node/add/sup_book');
     $I->see('Create Book');
     // check the fields
-    $I->see('Subtitle');
-    $I->see('Work ID');
-    $I->see('Book Authors');
-    $I->see('Authors (full)');
     $I->see('Author Info');
-    $I->see('Catalog Month');
-    $I->see('Catalog Page');
+    $I->see('Authors (full)');
+    $I->see('Availability Description');
+    $I->see('Awards');
+    $I->see('Book Authors');
+    $I->see('Book Image');
     $I->see('Catalog Season');
-    $I->see('Price Cloth');
-    $I->see('Price Paper');
-    $I->see('Price Digital');
     $I->see('Cloth Sale Percent');
     $I->see('Cloth Sale Price');
+    $I->see('Copublisher Name');
+    $I->see('Description');
+    $I->see('Digital Comp Link?');
+    $I->see('ISBN 13 Alternative');
+    $I->see('ISBN 13 Cloth');
+    $I->see('ISBN 13 Digital');
+    $I->see('ISBN 13 ISW');
+    $I->see('ISBN 13 Paper');
+    $I->see('Imprint');
+    $I->see('Imprint');
+    $I->see('Pages');
     $I->see('Paper Sale Percent');
     $I->see('Paper Sale Price');
-    $I->see('Web Code Cloth');
-    $I->see('Web Code Paper');
-    $I->see('Description');
-    $I->see('Availability Description');
-    $I->see('Copublisher Name');
-    $I->see('Copublisher?');
-    $I->see('Copyright Year');
-    $I->see('Digital Comp Link?');
-    $I->see('HTML Page Title');
-    $I->see('Illustrations');
-    $I->see('Imprint');
-    $I->see('In-Print Status');
-    $I->see('Instructor Recommended');
-    $I->see('ISBN 13 Cloth');
-    $I->see('ISBN 13 Paper');
-    $I->see('ISBN 13 Digital');
-    $I->see('ISBN 13 Alternative');
-    $I->see('ISBN 13 ISW');
-    $I->see('Local Web Blurb');
-    $I->see('New Paperback?');
-    $I->see('Pages');
+    $I->see('Price Cloth');
+    $I->see('Price Paper');
     $I->see('Print Desk Copies');
-    $I->see('Status Cloth');
-    $I->see('Status Paper');
-    $I->see('Status Digital');
     $I->see('Publication Date Cloth');
-    $I->see('Publication Date Paper');
-    $I->see('Publication Date Digital');
     $I->see('Publication Date First');
     $I->see('Publication Year First');
+    $I->see('Related Titles');
     $I->see('Reviews');
-    $I->see('Rights Description');
     $I->see('Sales Rank');
     $I->see('Series');
-    $I->see('URL ISW');
-    $I->see('Awards');
-    $I->see('Imprint');
     $I->see('Series');
-    $I->see('Related Titles');
-    $I->see('Book Image');
+    $I->see('Subtitle');
+    $I->see('URL ISW');
+    $I->see('Work ID');
   }
 
   /**
