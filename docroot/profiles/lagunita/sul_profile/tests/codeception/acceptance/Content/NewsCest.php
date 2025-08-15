b02d1a8231797775181f86c328c3b34f7f41ac65
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2025-08-15 15:44:18 -0700

    Move test function into the class
    Co-authored-by: pookmish <pookmish@gmail.com>

--- docroot/profiles/lagunita/sul_profile/tests/codeception/acceptance/Content/NewsCest.php
+++ docroot/profiles/lagunita/sul_profile/tests/codeception/acceptance/Content/NewsCest.php
@@ -255,14 +255,15 @@ class NewsCest {
     $I->assertEquals($values['featured_image_alt'], $I->grabAttributeFrom('meta[name="twitter:image:alt"]', 'content'), 'Metadata "twitter:image:alt" should match.');
   }
 
-}
 
-/**
- * Help text should appear on the news content authoring page for banner media field.
- */
-public function testBannerHelpText(AcceptanceTester $I) {
-  $I->logInWithRole('site_manager');
-  $I->amOnPage('/node/add/stanford_news');
-  $I->canSee('Maximum one banner media.');
-  $I->canSee('The "banner media" will display as a full-width image or video above the body text area on the article page. It will also be used as a thumbnail on the list page, and as a thumbnail on the teaser card paragraph if no Featured Media is added.');
+  /**
+   * Help text should appear on the news content authoring page for banner media field.
+   */
+  public function testBannerHelpText(AcceptanceTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/node/add/stanford_news');
+    $I->canSee('Maximum one banner media.');
+    $I->canSee('The "banner media" will display as a full-width image or video above the body text area on the article page. It will also be used as a thumbnail on the list page, and as a thumbnail on the teaser card paragraph if no Featured Media is added.');
+  }
+
 }
