99afffd5d317e0f3af38b37578e494a2d3d8b541
rebeccahongsf <rebeccahongsf@gmail.com>
2025-08-19 15:00:07 -0700

    shorten test phrase

--- docroot/profiles/lagunita/sul_profile/tests/codeception/acceptance/Content/NewsCest.php
+++ docroot/profiles/lagunita/sul_profile/tests/codeception/acceptance/Content/NewsCest.php
@@ -262,8 +262,6 @@ class NewsCest {
   public function testBannerHelpText(AcceptanceTester $I) {
     $I->logInWithRole('site_manager');
     $I->amOnPage('/node/add/stanford_news');
-    $I->canSee('Maximum one banner media.');
-    $I->canSee('The &quot;banner media&quot; will display as a full-width image or video above the body text area on the article page. It will also be used as a thumbnail on the list page, and as a thumbnail on the teaser card paragraph if no Featured Media is added.');
+    $I->canSee('It will also be used as a thumbnail on the list page');
   }
-
 }
