1d119d2376039218f1a3ba434044273b0d090aeb
joegl <6943710+joegl@users.noreply.github.com>
2025-10-03 11:07:32 -0500

    Use use statement for IntallerKernel.

--- docroot/sites/settings/global.settings.php
+++ docroot/sites/settings/global.settings.php
@@ -1,5 +1,6 @@
 <?php
 
+use Drupal\Core\Installer\InstallerKernel;
 use Drupal\SwsDrush\Helpers\EnvironmentDetector;
 
 /**
@@ -31,7 +32,7 @@ if (EnvironmentDetector::isAhEnv()) {
   // The increase is to handle drush site installs because the
   // install_config_import_batch step consumes upwards of 350+ MB of memory.
   // See: https://acquia.my.site.com/s/article/360004542293-Conditionally-increasing-memory-limits
-  if (Drupal\Core\Installer\InstallerKernel::installationAttempted()) {
+  if (InstallerKernel::installationAttempted()) {
     ini_set('memory_limit', '1024M');
   }
 }
