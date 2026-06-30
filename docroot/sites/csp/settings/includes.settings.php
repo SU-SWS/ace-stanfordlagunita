903a273cd36ab31c50099d38d9c818800602626a
Mike Decker <pookmish@stanford.edu>
2026-06-30 10:47:14 -0700

    CSP: Fixed next config override config

--- docroot/sites/csp/settings/includes.settings.php
+++ docroot/sites/csp/settings/includes.settings.php
@@ -16,7 +16,7 @@ elseif (EnvironmentDetector::isLocalEnv()) {
 }
 
 if ($next_domain) {
-  $config['next.next_site.vercel'] = [
+  $config['next.next_site.prod'] = [
     'base_url' => $next_domain,
     'preview_url' => "$next_domain/api/draft",
     'revalidate_url' => "$next_domain/api/revalidate",
