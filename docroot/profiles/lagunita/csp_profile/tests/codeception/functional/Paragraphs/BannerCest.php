15a762ab80325b012c6bbc3909ac4e1332009a04
Jen Breese-Kauth <jbreese@stanford.edu>
2026-07-21 15:48:16 -0700

    CSP-72: Changing names of button style and test name

--- docroot/profiles/lagunita/csp_profile/tests/codeception/functional/Paragraphs/BannerCest.php
+++ docroot/profiles/lagunita/csp_profile/tests/codeception/functional/Paragraphs/BannerCest.php
@@ -77,8 +77,8 @@ class BannerCest {
 
     // Unlimited cardinality & button type attribute.
     $I->canSeeElement( '[data-drupal-selector="edit-su-banner-button"] input[value="Add another item"]');
-    $I->canSeeOptionIsSelected('Button Type', 'Primary');
-    $I->selectOption('Button Type', 'Secondary');
+    $I->canSeeOptionIsSelected('Button Type', 'Solid red');
+    $I->selectOption('Button Type', 'Border brown');
 
     $I->selectOption('Text Overlay Position', 'Right');;
     $I->selectOption('Heading Level', 'h3');
