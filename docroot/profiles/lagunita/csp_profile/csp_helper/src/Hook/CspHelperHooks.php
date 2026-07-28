e78628ef633f56aff7f972f3d185c3bff266dccf
Ian Monroe <ian@ianmonroe.com>
2026-07-28 07:37:00 -0600

    CSP-101: Add new option to "Space below" select list on section Layout options (#269)
    * CSP-101: Add `ultra slim` option for space below in layout paragraphs layout
    
    * CSP-101: Add new option to Space below select list on section Layout options
    
    * refactor to replace and extend classes, added tests
    
    * Update docroot/profiles/lagunita/csp_profile/csp_helper/csp_helper.info.yml
    
    Co-authored-by: pookmish <pookmish@gmail.com>
    
    * added codeception test for ui
    
    ---------
    
    Co-authored-by: pookmish <pookmish@gmail.com>

--- docroot/profiles/lagunita/csp_profile/csp_helper/src/Hook/CspHelperHooks.php
+++ docroot/profiles/lagunita/csp_profile/csp_helper/src/Hook/CspHelperHooks.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Drupal\csp_helper\Hook;
 
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\csp_helper\Layouts\CspOneColumn;
+use Drupal\csp_helper\Layouts\CspThreeColumn;
+use Drupal\csp_helper\Layouts\CspTwoColumn;
 
 /**
  * Hook implementations for the csp_helper module.
@@ -29,6 +32,28 @@ class CspHelperHooks {
         'template' => 'paragraph--csp-course-card-instructor',
       ],
     ];
+  }
+
+  /**
+   * Implements hook_layout_alter().
+   *
+   * Swaps the upstream stanford_layout_paragraphs layout plugins for CSP
+   * subclasses that add an "Ultra slim" (~6px) option to the "Space below
+   * section" (bottom_margin) select. Overriding the plugin class keeps the
+   * change scoped to the CSP site.
+   */
+  #[Hook('layout_alter')]
+  public function layoutAlter(array &$definitions): void {
+    $class_map = [
+      'layout_paragraphs_1_column' => CspOneColumn::class,
+      'layout_paragraphs_2_column' => CspTwoColumn::class,
+      'layout_paragraphs_3_column' => CspThreeColumn::class,
+    ];
+    foreach ($class_map as $layout_id => $class) {
+      if (isset($definitions[$layout_id])) {
+        $definitions[$layout_id]->setClass($class);
+      }
+    }
   }
 
 }
